a4b54e3b3fab97cbb431c29dae6985a2cc6dfeda
kelliaUmuhire <kellumuhire@gmail.com>
2024-07-06 14:23:26 -0700

    Tasks

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""get subscribers numbers function"""
-
-
-import json
+""" 0-subs.py """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """get all subscribers"""
-    if len(sys.argv) < 2:
-        return 0
+    """ number_of_subscribers """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
     else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
-            return 0
-        body = json.loads(result.text)
-        return body["data"]["subscribers"]
+        return 0
--- api_advanced/README.md
+++ api_advanced/README.md
@@ -1,1 +1,1 @@
-readme
+## this is my api advanced in python
